ebed9b4ea53d9ba28acae402e312e59a33609b32
Trang Pham <ttpham@wpi.edu>
2021-10-04 20:20:32 -0400

    add the basic arguments check and switch

--- p3/src/DBConnect.java
+++ p3/src/DBConnect.java
@@ -1,2 +1,65 @@
+import java.sql.*;
+
 public class DBConnect {
+    //private static final String USERID = "ttpham";
+    //private static final String PASSWORD = "Thutrang68";
+    private Connection connection;
+
+    public boolean ConnectDB( String UserID, String Password){
+        System.out.println("-------Oracle JDBC COnnection Testing ---------");
+        try {
+            // Register the Oracle driver
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        } catch (ClassNotFoundException e){
+            System.out.println("Where is your Oracle JDBC Driver?");
+            e.printStackTrace();
+            return false;
+        }
+
+        System.out.println("Oracle JDBC Driver Registered!");
+        connection = null;
+
+        try {
+            // create the connection string
+            connection = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl", UserID, Password);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return false;
+        }
+        System.out.println("Oracle JDBC Driver Connected!");
+        return true;
+    }
+
+    public void getGallery() {
+        try {
+            Statement stmt = connection.createStatement();
+            String str = "SELECT * FROM GALLERY";
+            ResultSet rset = stmt.executeQuery(str);
+
+            String galleryName = "";
+            String buildingName = "";
+            int maxCapacity = 0;
+            // Process the results
+
+            while (rset.next()) {
+                maxCapacity = rset.getInt("maxCapacity");
+                galleryName = rset.getString("galleryname");
+                buildingName = rset.getString("buildingname");
+
+                System.out.println("Gallery Name: " + galleryName + "  Building Name: " + buildingName + "   Max Capacity: " + maxCapacity);
+            } // end while
+
+            rset.close();
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Get Data Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+    }
+
 }
--- p3/src/p3.java
+++ p3/src/p3.java
@@ -2,5 +2,64 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class p3 {
+
+    private static DBConnect db;
+
     public static void main(String[] args) {
+
+        if (args.length <2)
+        {
+            System.out.println("You need to include your UserID and Password parameters on the command line");
+            return;
+        }
+
+        else if (args.length != 3 )
+        {
+            System.out.println("Include the number of the following menu item as the third parameter on the command line.\n" +
+                    "\n" +
+                    "1 – Report Participant Information\n" +
+                    "2 – Report Pottery Information\n" +
+                    "3 – Report Building Galleries Information\n" +
+                    "4 – Update Member ID\n");
+            return;
+        }
+
+        //create database connection if not there already
+        if (db == null) {
+            db = new DBConnect();
+            if (!db.ConnectDB(args[0], args[1])) {
+                return;
+            }
+        }
+
+        //evaluate data
+        switch (args[2]) {
+            case "1":
+                System.out.println("Enter Participant’s email address: <and wait for user’s input>");
+                //add db function
+                break;
+            case "2":
+                System.out.println("Enter Pottery ID: <and wait for user’s input>");
+                //add db function
+                break;
+
+            case "3":
+                System.out.println("Enter Building Name: <and wait for user’s input>");
+                //add db function
+                break;
+
+            case "4":
+                System.out.println("Enter the Participant’s Email Address: <and wait for user’s input>\n" +
+                                "Enter the updated Member ID: <and wait for user’s input>\n");
+                //add db function
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + args[2]);
+        }
+        return;
+    }
+
 }
+
+
